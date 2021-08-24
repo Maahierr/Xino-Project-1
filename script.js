967f311cff6e009a0d5a5d2934ef7f1687ddfe9b
Maahierr <82525255+Maahierr@users.noreply.github.com>
2021-08-24 13:51:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("sidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
     document.getElementById("main").style.visibility = "hidden";
   }
   
   function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("sidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
     document.getElementById("main").style.visibility= "visible";
   }
