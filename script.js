b7777841b1447287dd2b2dbdc836ccd961e5b6b9
Maahierr Khanna <maahierrkhanna06@gmail.com>
2021-08-24 18:30:03 +0530

    2

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+var  up = document.getElementById('up');
+
 function openNav() {
     document.getElementById("sidebar").style.width = "250px";
     document.getElementById("openbtn").style.marginLeft = "250px";
     document.getElementById("openbtn").style.visibility = "hidden";
-  }
+}
   
-  function closeNav() {
+function closeNav() {
     document.getElementById("sidebar").style.width = "0";
     document.getElementById("openbtn").style.marginLeft= "0";
     document.getElementById("openbtn").style.visibility= "visible";
+}
+
+window.onscroll = function(){
+  if(document.documentElement.scrollTop > 18 || document.body.scrollTop > 18){
+    up.style.visibility = "visible";
   }
+  else{
+    up.style.visibility = "hidden";
+  }
+}
