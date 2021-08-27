84922cd3431310e3528c8fdc88223f9e64867d1d
Maahierr Khanna <maahierrkhanna06@gmail.com>
2021-08-27 17:30:54 +0530

    major

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 var  up = document.getElementById('up');
+var img = document.createElement("img");
+var src = document.getElementById("visibleImg");
+
+img.style.width = "250px";
+img.style.height = "250px";
+
+var i1 = document.getElementById('i1');
+var i2 = document.getElementById('i2');
+var i3 = document.getElementById('i3');
+var i4 = document.getElementById('i4');
+var i5 = document.getElementById('i5');
 
 function openNav() {
     document.getElementById("sidebar").style.width = "250px";
@@ -18,5 +29,32 @@ window.onscroll = function(){
   }
   else{
     up.style.visibility = "hidden";
+  }
+}
+
+function visibleImg(){
+  if(i1.onclick){
+    img.src = "media/1.jpg";
+    src.appendChild(img);
+  }
+
+  else if(i2.onclick){
+    img.src = "media/2.jpg";
+    src.appendChild(img);
+  }
+
+  else if(i3.onclick){
+    img.src = "media/3.jpg";
+    src.appendChild(img);
+  }
+
+  else if(i4.onclick){
+    img.src = "media/4.jpg";
+    src.appendChild(img);
+  }
+
+  else if(i5.onclick){
+    img.src = "media/5.jpg";
+    src.appendChild(img);
   }
 }
